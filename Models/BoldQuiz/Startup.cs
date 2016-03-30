7c37b54d6c4e1d4a7865488b3918e218e45a856d
Umut93 <umutkayatuz@live.dk>
2016-03-30 11:35:26 +0200

    MVC

---
+++ Models/BoldQuiz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoldQuiz.Startup))]
+namespace BoldQuiz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
