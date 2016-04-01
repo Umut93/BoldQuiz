6041d7b64e33cd57ff557e431e9fc4bff0cf50e1
Umut93 <umutkayatuz@live.dk>
2016-04-01 12:00:10 +0200

    Added Dapper on DAL

---
+++ BoldQuizMVC/DAL/AnswerRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Models;
+using System.Configuration;
+
+namespace DAL
+{
+   public class AnswerRepository
+    {
+        private SqlConnection con;
+
+        public AnswerRepository()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+        }
+            
+    }
+}
