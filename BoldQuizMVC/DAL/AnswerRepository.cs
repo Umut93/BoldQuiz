968997c4bf15a83d3cdb7ed410b48a60d3e9ca9b
Umut93 <umutkayatuz@live.dk>
2016-04-01 12:26:03 +0200

    Solved (dapper + identityCore)

--- BoldQuizMVC/DAL/AnswerRepository.cs
+++ BoldQuizMVC/DAL/AnswerRepository.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 using Models;
+using Dapper;
 using System.Configuration;
 
 namespace DAL
