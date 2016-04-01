02aff5a16634ab89183756bfc885eeeaeb346068
Umut93 <umutkayatuz@live.dk>
2016-04-01 23:17:36 +0200

    d

--- BoldQuizMVC/DAL/UserRepository.cs
+++ BoldQuizMVC/DAL/UserRepository.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using Dapper;
 using System.Threading.Tasks;
+using IDAL;
+using System.Configuration;
 
 namespace DAL
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private SqlConnection con;
+
+       // public UserRepository()
+        //{
+         //   string connectString = ConfigurationManager.
+           // this.con = new SqlConnection();
+
+        }
     }
-}
+//}
--- BoldQuizMVC/IDAL/IUserRepository.cs
+++ BoldQuizMVC/IDAL/IUserRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IDAL
 {
-    public class Class1
+   public interface IUserRepository
     {
     }
 }
