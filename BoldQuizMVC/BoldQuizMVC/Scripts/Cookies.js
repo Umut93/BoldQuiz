f60378bf8e6c0949b2782eaf767329846518751c
Umut93 <umutkayatuz@live.dk>
2016-05-12 10:39:38 +0200

    Cookies comments

--- BoldQuizMVC/BoldQuizMVC/Scripts/Cookies.js
+++ BoldQuizMVC/BoldQuizMVC/Scripts/Cookies.js
@@ -14,7 +14,7 @@ function handleSeleteced()
 }
 
 //http://www.w3schools.com/js/js_cookies.asp = 10 days
-
+//Setting a cookie by its name, value and expiredays. The values are the values in the array which is selected possible answers (ID)!
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (10 * 24 * 60 * 60 * 1000));
@@ -22,6 +22,11 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + "; " + expires;
 }
 //Get cookie http://www.w3schools.com/js/js_cookies.asp
+// Return the value of specified cookie.
+//35 if cookies is found otherwise return "".
+//charat takes a specific character from a string.
+//substring takes character from a string in specific string.
+//indexOF?
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
