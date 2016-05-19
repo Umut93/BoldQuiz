9e3283fcf4f6734561adb231bb9cd63398cad854
Umut93 <umutkayatuz@live.dk>
2016-05-19 11:31:07 +0200

    comments

--- BoldQuizMVC/BoldQuizMVC/Scripts/Invites.js
+++ BoldQuizMVC/BoldQuizMVC/Scripts/Invites.js
@@ -2,9 +2,9 @@
 //$(selector)
 //JSON location : contoller/actionName
 //URL(specifies the url to send the request to), data (specifies data to be sent to the server), sucess(if the request is sucedded run)
+//Load data from the server using GET HTTP request
 //Null = vi skal ikke sende noget brugerId afsted? 
 //Selector!
-
 
 $.getJSON("Invite/findInvitesForOnePerson", null, function (data)
 {
